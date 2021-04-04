fee03ca6197b06654db596f822ad5debe8f7bdfc
yonjjang <fic1214@gmail.com>
2021-04-04 23:54:32 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var app = http.createServer(function(request,response){
+    var _url = request.url;
+    console.log(_url);
+    var queryData = url.parse(_url, true).query;
+    console.log(queryData);
+    if(_url == '/'){
+      _url = '/index.html';
+    }
+    if(_url == '/favicon.ico'){
+      return response.writeHead(404);
+    }
+    response.writeHead(200);
+    //console.log(__dirname + _url);
+    response.end(fs.readFileSync(__dirname + _url));
+ 
+});
+app.listen(3000);
